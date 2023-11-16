adf06cc4029c287cc063c187b9f03f02aea87414
Binozo <entwickler@binozoworks.de>
2023-11-16 16:31:47 +0100

    added BestDeviceConfig api

--- pkg/tinyalsa/device.go
+++ pkg/tinyalsa/device.go
@@ -2,6 +2,7 @@ package tinyalsa
 
 import (
 	"github.com/Binozo/GoTinyAlsa/internal/tinyapi"
+	"github.com/Binozo/GoTinyAlsa/internal/tinypcm"
 	"github.com/Binozo/GoTinyAlsa/pkg/pcm"
 )
 
@@ -16,6 +17,28 @@ func NewDevice(cardNr int, deviceNr int, deviceConfig pcm.Config) AlsaDevice {
 		Card:         cardNr,
 		Device:       deviceNr,
 		DeviceConfig: deviceConfig,
+	}
+}
+
+func BestDeviceConfig(cardNr int, deviceNr int, format int) pcm.Config {
+	device := AlsaDevice{
+		Card:         cardNr,
+		Device:       deviceNr,
+		DeviceConfig: pcm.Config{},
+	}
+	info := device.GetInfo()
+	infoData := info.In
+	if format == PCM_IN {
+		infoData = info.In
+	} else {
+		infoData = info.Out
+	}
+	return pcm.Config{
+		Channels:    int(infoData.ChannelsMax),
+		SampleRate:  int(infoData.RateMax),
+		PeriodSize:  int(infoData.PeriodSizeMax),
+		PeriodCount: int(infoData.PeriodCountMax),
+		Format:      tinypcm.Format(format),
 	}
 }
 
