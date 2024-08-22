a975e86647a26a8335aa5426a40d7371ec6d7407
Eric Lewis <ericlewis777@gmail.com>
2024-08-22 14:09:47 -0400

    expose more pcm information

--- pkg/tinyalsa/audiosession.go
+++ pkg/tinyalsa/audiosession.go
@@ -1,6 +1,10 @@
 package tinyalsa
 
-import "github.com/Binozo/GoTinyAlsa/internal/tinyapi"
+import (
+	"time"
+
+	"github.com/Binozo/GoTinyAlsa/internal/tinyapi"
+)
 
 type AudioSession struct {
 	pcmDevice tinyapi.PcmDevice
@@ -13,7 +17,6 @@ func (d *AlsaDevice) NewAudioSession() (AudioSession, error) {
 	if err != nil {
 		return AudioSession{}, err
 	}
-
 	return AudioSession{
 		pcmDevice: pcmDevice,
 	}, nil
@@ -29,7 +32,37 @@ func (a *AudioSession) BufferSize() int {
 	return a.pcmDevice.FrameBytesSize()
 }
 
+// BytesPerFrame returns the number of bytes per frame
+func (a *AudioSession) BytesPerFrame() int {
+	return a.pcmDevice.BytesPerFrame()
+}
+
+// BitsPerSample returns the number of bits per sample
+func (a *AudioSession) BitsPerSample() uint16 {
+	return a.pcmDevice.BitsPerSample()
+}
+
+// IsReady checks if the device is ready
+func (a *AudioSession) IsReady() bool {
+	return a.pcmDevice.IsReady()
+}
+
+// WaitUntilReady waits until the device is ready or timeout occurs
+func (a *AudioSession) WaitUntilReady(timeout time.Duration) error {
+	return a.pcmDevice.WaitUntilReady(timeout)
+}
+
 // Pump the audio data into the device.
 func (a *AudioSession) Pump(data []byte) error {
 	return a.pcmDevice.WriteFrames(data, len(data))
+}
+
+// Read reads audio data from the device
+func (a *AudioSession) Read(buffer []byte) error {
+	return a.pcmDevice.ReadFrames(buffer, len(buffer))
+}
+
+// GetError returns the current error state of the device
+func (a *AudioSession) GetError() error {
+	return a.pcmDevice.GetError()
 }
